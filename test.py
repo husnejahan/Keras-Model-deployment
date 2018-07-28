4b8473b3fb477f35106baf121e5cbf3b0d6d5144
Most Husne Jahan <mosthusne.jahan@ryerson.ca>
2018-07-27 21:43:20 -0400

    Update and rename img-reco-test.py to test.py

--- test.py
+++ test.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-
-
 @author: most husne jahan
 """
 
-# For Genrating test images
-#from PIL import Image
-#from keras.datasets import mnist
-#import numpy as np
-#
-#(X_train, y_train), (X_test, y_test) = mnist.load_data()
-#for i in np.random.randint(0, 10000+1, 10):
-#    arr2im = Image.fromarray(X_train[i])
-#    arr2im.save('{}.png'.format(i), "PNG")
-
-
-
 ## API building
+# Simple Flask API
 
 #tf.keras.backend.clear_session()
 
